6216c73502b634e527604f8dd31935afabb9c661
Bruce Webber <self@brucewebber.us>
2017-03-18 11:19:15 -0400

    Add comments to SQLAlchemy version of create_db.py

--- sqlalchemy/create_db.py
+++ sqlalchemy/create_db.py
@@ -10,6 +10,7 @@ session = Session()
 
 # Create the database.
 Base.metadata.create_all(engine)
+print('Created the database and tables')
 
 # John Adams
 person = Person(first_name='John', last_name='Adams')
@@ -18,16 +19,20 @@ person.phones.append(phone)
 phone = Phone(phone_number='555-5555', phone_type='cell')
 person.phones.append(phone)
 session.add(person)
+print('Added info for John Adams')
 
 # Sara Smith
 person = Person(first_name='Sara', last_name='Smith')
 phone = Phone(phone_number='555-9999', phone_type='cell')
 person.phones.append(phone)
 session.add(person)
+print('Added info for Sara Smith')
 
 # Richard Recluse
 person = Person(first_name='Richard', last_name='Recluse')
 session.add(person)
+print('Added info for Richard Recluse')
 
 # Commit the records to the database.
 session.commit()
+print('Committed changes to the database')
